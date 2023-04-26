1bd683cf229d07804177222533c172d8bb77996b
youshenlin <mikey8316@gmail.com>
2023-04-26 23:31:39 +0800

    fix

--- build_ocr.sh
+++ build_ocr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 systemctl start docker
 docker build -t ocr_server:v1 . --no-cache
-docker run -p 80:9898 -d ocr_server:v1
+docker run --name ocr -p 80:9898 -d ocr_server:v1
 echo "啟動 ocr port 80"

68b6216e8be83c0efa7dc34a693983d53cd52d59
YouSheng Lin <mikey8316@gmail.com>
2023-04-27 18:10:52 +0800

    fix: docker 自動啟動

--- build_ocr.sh
+++ build_ocr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 systemctl start docker
+systemctl enable docker.service
 docker build -t ocr_server:v1 .
-docker run --name ocr -p 80:9898 -d ocr_server:v1
+docker run --name ocr -p 80:9898 -d --restart=always ocr_server:v1
 echo "啟動 ocr port 80"

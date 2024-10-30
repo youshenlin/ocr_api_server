404270daf3b43e5f88f7f49a36392937ae4d8020
youshenlin <mikey8316@gmail.com>
2024-10-30 15:50:56 +0800

    feat: windows 版本

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
+REM Build Docker image with tag "ocr_server:v1"
 docker build -t ocr_server:v1 .
-docker run -p 5555:9898 -d ocr_server:v1
+
+REM Run Docker container with the name "ocr" and publish ports
+docker run --name ocr -p 5555:9898 -d --restart=always ocr_server:v1
+
+REM Output message
 echo 啟動 ocr port 5555
-pause
 
